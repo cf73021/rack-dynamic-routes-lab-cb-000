24ee84db8665fa4aaa0161916da0ed272173dde2
Chris Franklin <cf73021@gmail.com>
2018-10-19 20:09:08 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,10 +7,8 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-
       item_name = req.path.split("/items/").last #turn /songs/Sorry into Sorry
       item = @@items.find{|i| i.name == item_name}
-
       resp.write item.name
     end
 
