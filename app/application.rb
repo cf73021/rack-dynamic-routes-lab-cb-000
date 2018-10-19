c856eaede44e0f3639356da917d7a4d3b34f3ea9
Chris Franklin <cf73021@gmail.com>
2018-10-19 20:18:40 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,14 +1,14 @@
 class Application
 
-  @@item = []
+  @@items = []
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      item_name = req.path.split("/items/").last 
-      item = @@item.find{|i| i.name == item_name}
+      item_name = req.path.split("/items/").last
+      item = @@items.find{|i| i.name == item_name}
       resp.write item.name
     else
       resp.write "Route not found"
