a3261306395bf29fbc857e1e23a02c0b00c6356a
Chris Franklin <cf73021@gmail.com>
2018-10-19 20:07:23 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     if req.path.match(/items/)
 
       item_name = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
-      item = @@items.find{|i| i.name == song_title}
+      item = @@items.find{|i| i.name == item_name}
 
       resp.write song.artist
     end
