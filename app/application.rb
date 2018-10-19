d1be71fd7b251eb77efe9d5125d186d7036f1ca5
Chris Franklin <cf73021@gmail.com>
2018-10-19 20:17:15 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,8 +8,8 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last #turn /songs/Sorry into Sorry
-      item = @@items.find{|i| i.name == item_name}
-      resp.write items.name
+      item = @@item.find{|i| i.name == item_name}
+      resp.write item.name
     else
       resp.write "Route not found"
       resp.status = 404
