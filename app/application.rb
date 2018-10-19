4e28e3c756d53d624020f9ca489eaf353baa3178
Chris Franklin <cf73021@gmail.com>
2018-10-19 20:06:32 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,13 +1,13 @@
 class Application
 
   @@items = [Item.new("Sorry", "Justin Bieber"),
-            Song.new("Hello","Adele")]
+            Item.new("Hello","Adele")]
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/songs/)
+    if req.path.match(/items/)
 
       song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
       song = @@songs.find{|s| s.title == song_title}
